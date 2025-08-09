660c310c40a6ea79e38164d2cb4c40d1aeb2dd49
Ahad <muhammadahad211126@gmail.com>
2025-08-09 12:42:42 +0500

    19: Meetings Filters

--- src/modules/agents/ui/components/list-header.tsx
+++ src/modules/agents/ui/components/list-header.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 import { PlusIcon, XCircleIcon } from 'lucide-react'
 import React, { useState } from 'react'
 import NewAgentDialog from './new-agent-dialog'
-import { useAgentsFilters } from '@/hooks/use-agents-filters'
+import { useAgentsFilters } from '@/modules/agents/hooks/use-agents-filters'
 import AgentSearchFilters from './agents-search-filters'
 import { DEFAULT_PAGE } from '@/constants'
 
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -7,17 +7,32 @@ import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
 import React from 'react'
 import { columns } from '../components/columns';
 import EmptyState from '@/components/empty-state';
+import { useMeetingsFilters } from '../../hooks/use-meetings-filters';
+import DataPagination from '@/components/data-pagination';
+import { useRouter } from 'next/navigation';
 
 const MeetingsView = () => {
     const trpc = useTRPC();
+    const [filter,setFilters] = useMeetingsFilters()
+    const router = useRouter()
     
-    const {data} = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}))
-    console.log(data)
+    const {data} = useSuspenseQuery(trpc.meetings.getMany.queryOptions({
+      ...filter
+    }))
   return (
     <div className='flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4'>
       <DataTable 
         columns={columns}
-        data = {data.items}/>
+        data = {data.items}
+        onRowClick={(row) => router.push(`/meetings/${row.id}`)}
+        />
+
+      <DataPagination
+        page={filter.page}
+        totalPages={data.totalPages}
+        onPageChange={(page) => setFilters({page})}
+
+      />
         {data.items.length === 0 && (
             <EmptyState
               title='Create Your First Meeting '
